c5372941904bc5dd7c778187251fc9791a576baf
Gabriel Corteletti <gabriellcorteletti@gmail.com>
2023-12-12 17:30:02 +0100

    fixed led not showing change for slow

--- labs/lab04_ex01/lab04_ex01.cpp
+++ labs/lab04_ex01/lab04_ex01.cpp
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include "Arduino.h"
 #include "tpl_os.h"
-#include <string.h>
-#include <stdlib.h>
 
 #define K 5    //queue size
 
@@ -31,6 +29,7 @@ DeclareResource(Sem);
 
 TASK (TaskS){
     static int X;
+    char out_S[64];
 
     //Get analog value of voltage source
     X = analogRead(A0);
@@ -38,9 +37,11 @@ TASK (TaskS){
     //Checks error
     if(X<10 || X>1013){
         error = 1;
+        sprintf(out_S, "S: New X = %d -> ERROR",X);
     }
     else{
         error = 0;
+        sprintf(out_S, "S: New X = %d",X);
     }
 
     //Checks for overflow
@@ -51,19 +52,14 @@ TASK (TaskS){
         //Critical Section begins
         GetResource(Sem);
 
-        Q[add] = X;         //Inserts new X in Q
-
-        //Prints new measured X
-        char msg[64];
-        sprintf(msg, "S: New X = %d -> err: %d",X,error);
-        Serial.println(msg);
-
-        num++;              //Increment amount of data
+        Q[add] = X;                 //Inserts new X in Q
+        Serial.println(out_S);      //Prints new measured X
+        num++;                      //Increment amount of data
 
         //Critical Section ends
         ReleaseResource(Sem);
 
-        add = (add+1)%K;    //circular buffer: 0 <= add <= K-1
+        add = (add+1)%K;            //circular buffer: 0 <= add <= K-1
     }
 
     TerminateTask();
@@ -73,6 +69,7 @@ TASK (TaskS){
 TASK (TaskB){
     int M = -9999;   //maximum
     int N = 9999;    //minimum
+    char out_B[64];
 
     for(int i=0; i<K; i++){     //Reads all elements in Q
         if(num>0){              //if there are any left
@@ -102,15 +99,15 @@ TASK (TaskB){
     //Checks alarm
     if(M-N > 500){
         alarm = 1;
+        sprintf(out_B, "\t\tB: N = %d  |  M = %d\n\t\t   M-N = %d -> ALARM", N, M, M-N);
     }
     else{
         alarm = 0;
+        sprintf(out_B, "\t\tB: N = %d  |  M = %d\n\t\t   M-N = %d", N, M, M-N);
     }
 
     //Print B results
-    char msg[64];
-    sprintf(msg, "\t\tB: N = %d  |  M = %d\n\t\t   M-N = %d -> alarm: %d", N, M, N-M, alarm);
-    Serial.println(msg);
+    Serial.println(out_B);
 
     TerminateTask();
 }
@@ -119,19 +116,29 @@ TASK (TaskB){
 TASK (TaskV){
     static int led_state = 0;
     static int countV = 0;
+    static int first_slow = 1;
 
-    //Update state
-	if (error == 1){					                //fast
+    //Update state:
+
+	if (error){					        //fast
 		digitalWrite(13, led_state);
 		led_state = !led_state;
+        first_slow = 1;
 	}
-	else if (error == 0 && alarm == 1 && countV >= 4){	//slowly
+  
+	else if (alarm && countV >= 4){     //slowly
+        if (first_slow){                //if it's the first instance of "slow"
+            led_state = 1;              //starts blinking with led ON
+        }                               //so the change can always be seen
 		digitalWrite(13, led_state);
-		led_state = !led_state;
+        led_state = !led_state;
 		countV = 0;
+        first_slow = 0;
 	}
-	else if (error == 0 && alarm == 0){					//OFF
+
+    else if (!alarm){		            //OFF
 		digitalWrite(13, LOW);
+        first_slow = 1;
 	}
 
     countV++;
