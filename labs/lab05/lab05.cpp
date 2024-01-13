ff156f844df7b550584560aa34ee013e66516989
Gabriel Corteletti <gabriellcorteletti@gmail.com>
2024-01-13 17:50:17 +0100

    solution w/ prints

--- labs/lab05/lab05.cpp
+++ labs/lab05/lab05.cpp
@@ -2,16 +2,16 @@
 #include "Arduino.h"
 #include "tpl_os.h"
 
-#define K 67                //queue size
+#define K 67                                //queue size
 
 //Global variables
-int Q[K];                   //shared queue
-unsigned int num = 0;       //amount of data in Q
+int Q[K];                                   //shared queue
+unsigned int num = 0;                       //amount of data in Q
 
 
 void setup()
 {
-    pinMode(A0, INPUT);     //signal
+    pinMode(A0, INPUT);                     //signal
     Serial.begin(9600);
 }
 
@@ -24,22 +24,22 @@ DeclareResource(Sem);
 
 
 TASK (TaskS){
-    static int msgS = 1;            //message to activate consumer
-    static int S;                   //sample of the signal
-    static int add = 0;             //position to add next element
+    static int msgS = 1;                    //message to activate consumer
+    static int S;                           //sample of the signal
+    static int add = 0;                     //position to add next element
 
-    S = analogRead(A0);             //get a new sample of the signal
+    S = analogRead(A0);                     //get a new sample of the signal
 
-    GetResource(Sem);               //Critical Section begins
+    GetResource(Sem);                       //Critical Section begins
 
     //Checks for overflow
     if (num >= K){
         Serial.println("ERROR: Queue Overflow");
-        ReleaseResource(Sem);       //Critical Section ends
+        ReleaseResource(Sem);               //Critical Section ends
     }
     else{
-        Q[add] = S;                 //inserts new S in Q
-        num++;                      //increment amount of data
+        Q[add] = S;                         //inserts new S in Q
+        num++;                              //increment amount of data
 
         //prints new data
         Serial.print("S: ");       
@@ -53,9 +53,9 @@ TASK (TaskS){
 	        SendMessage(msgS_send, &msgS);
         }
 
-        ReleaseResource(Sem);       //Critical Section ends
+        ReleaseResource(Sem);               //Critical Section ends
 
-        add = (add+1)%K;            //circular buffer: 0 <= add <= K-1
+        add = (add+1)%K;                    //circular buffer: 0 <= add <= K-1
     }
 
     TerminateTask();
@@ -63,7 +63,7 @@ TASK (TaskS){
 
 
 TASK (TaskC){
-    static int rem = 0;             //position to remove next element
+    static int rem = 0;                     //position to remove next element
     static int next_rem;
     static int prev_rem;
     static int s_count = 0;
@@ -71,76 +71,59 @@ TASK (TaskC){
     float RR;
     float HR;
     static int peak_flag = 0;
+    static int psbl_peak = -1;
 
 
-    for(int i=0; i<50; i++){        //reads 50 elements in Q
+    for(int i=0; i<50; i++){                //reads 50 elements in Q
 
-        next_rem = (rem + 1)%K;
-        prev_rem = (rem - 1 + K)%K;
+        next_rem = (rem + 1)%K;             //next sample
+        prev_rem = (rem - 1 + K)%K;         //previous sample
 
-        GetResource(Sem);               //Critical Section begins
+        GetResource(Sem);                   //Critical Section begins
 
-        // if (i<49) {         //if (i>1 && i<49) {          //discards the first and last of the 50 samples
-        //     if ((Q[rem] > Q[prev_rem]) && (Q[rem] > Q[next_rem])) {
-        //         peak_flag = 1;
-        //     }
-        // }
-        // else if ((Q[rem] > Q[prev_rem]) && (Q[rem] > 1000)) {
+        // if (((i<49) && (Q[rem] > Q[prev_rem]) && (Q[rem] >= Q[next_rem])) ||
+        //     ((i==49) && (Q[rem] > Q[prev_rem]) && (Q[rem] > 1000))) {         //if (i>1 && i<49) {          //discards the first and last of the 50 samples
         //     peak_flag = 1;
+        //     Serial.print("\t\tPEAK: ");
+        //     Serial.println(Q[rem]);
         // }
 
-        if (((i<49) && (Q[rem] > Q[prev_rem]) && (Q[rem] > Q[next_rem])) ||
-            ((i==49) && (Q[rem] > Q[prev_rem]) && (Q[rem] > 1000))) {         //if (i>1 && i<49) {          //discards the first and last of the 50 samples
-            // peak_flag = 1;
+        if ((i==0) && (psbl_peak >= Q[rem])) {                              //check if possible peak is
+            peak_flag = 1;                                                  //indeed an actual peak
+            Serial.print("\t\t###POSSIBLE PEAK: ");
+            Serial.println(psbl_peak);
+        }
+        psbl_peak = -1;                                                     //resets possible peak
+        if ((i<49) && (Q[rem] > Q[prev_rem]) && (Q[rem] >= Q[next_rem])) {  //find peaks
+            peak_flag = 1;
             Serial.print("\t\tPEAK: ");
             Serial.println(Q[rem]);
-            if (!first_peak) {
+        }
+        else if ((i==49) && (Q[rem] > Q[prev_rem]) && (Q[rem] > 900)) {     //special case for last sample
+            psbl_peak = Q[rem];                                             //save possible peak
+        }
+
+        num--;                              //decrement amount of data
+
+        ReleaseResource(Sem);               //Critical Section ends
+
+        if (peak_flag) {                    //if a peak was found
+            if (!first_peak) {              //check if it's not the 1st
                 RR = s_count*0.02;
                 HR = 60/RR;
-                // Serial.print("\t\tRR: ");
-                // Serial.print(RR);
-                // Serial.print("  |  HR: ");
                 Serial.print("\t\tHR: ");
                 Serial.println(HR);
             }
-            else {
+            else {                          //updates the first flag
                 first_peak = 0;
             }
-            s_count = 0;
+
+            s_count = 0;                    //resets sample counter
+            peak_flag = 0;                  //reset peak flag
         }
 
-
-
-
-
-
-        // if (peak_flag) {
-        //     Serial.print("\t\tPEAK: ");
-        //     Serial.println(Q[rem]);
-        //     if (!first_peak) {
-        //         RR = s_count*0.02;
-        //         HR = 60/RR;
-        //         // Serial.print("\t\tRR: ");
-        //         // Serial.print(RR);
-        //         // Serial.print("  |  HR: ");
-        //         Serial.print("\t\tHR: ");
-        //         Serial.println(HR);
-        //     }
-        //     else {
-        //         first_peak = 0;
-        //     }
-        //     s_count = 0;
-        //     peak_flag = 0;
-        // }
-
-
-
-        num--;                      //decrement amount of data
-
-        ReleaseResource(Sem);       //Critical Section ends
-
-        s_count++;                  //increment sample counter
-        rem = next_rem; //(rem+1)%K;            //circular buffer: 0 <= rem <= K-1
+        s_count++;                          //increment sample counter
+        rem = next_rem;                     //circular buffer: 0 <= rem <= K-1
     }
 
     TerminateTask();
